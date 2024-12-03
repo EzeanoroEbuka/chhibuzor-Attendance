aed994edf7af293486576e92f4e1c51da63aea64
EzeanoroEbuka <chukwuebukaezeanoro2@gmail.com>
2024-12-03 06:01:28 +0100

    start up of our attendance project

---
+++ src/main/java/com/attendance/ChibuzorAttendance/ChibuzorAttendanceApplication.java
@@ -0,0 +1,13 @@
+package com.attendance.ChibuzorAttendance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChibuzorAttendanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChibuzorAttendanceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/attendance/ChibuzorAttendance/ChibuzorAttendanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.attendance.ChibuzorAttendance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChibuzorAttendanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
